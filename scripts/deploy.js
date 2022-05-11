e2f3c7f530d3a3b76db2f33dae183bb29a83c5ef
Kate Johnson <johnson.kat.g@gmail.com>
2022-05-11 19:51:43 -0400

    commented deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,15 +3,16 @@ const fs = require('fs');
 
 async function main() {
   const contractName = 'myVault';
-  await hre.run("compile");
+  await hre.run("compile"); // compile everytime changes are made
   const smartContract = await hre.ethers.getContractFactory(contractName);
   const myVault = await smartContract.deploy();
   await myVault.deployed();
   console.log(`${contractName} deployed to: ${myVault.address}`);
 
-  const contractArtifacts = await artifacts.readArtifactSync(contractName);
-  fs.writeFileSync('./artifacts/contractArtifacts.json',  JSON.stringify(contractArtifacts, null, 2));
-
+  const contractArtifacts = await artifacts.readArtifactSync(contractName); 
+  fs.writeFileSync('./artifacts/contractArtifacts.json', JSON.stringify(contractArtifacts, null, 2)); // write artifacts to external file
+  
+  // veirfy the contract was successfully created on Etherscan
   await hre.run("verify:verify", {
     address: myVault.address,
     //constructorArguments: [],
